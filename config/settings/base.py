093519a477e150f7162a9a896eb2adc984892c3c
Hemang Mohan <44643029+hemangsrr@users.noreply.github.com>
2019-12-17 21:13:31 +0530

    updated ALLOWED_HOST to  include heroku

--- config/settings/base.py
+++ config/settings/base.py
@@ -194,7 +194,6 @@ STATIC_URL = '/static/'
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
-    '/invento18/static'
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
